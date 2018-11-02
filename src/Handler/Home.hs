25942469919ded2b727455a7ac76a51ccc6b806d
Matheus Jacques <Matheus Jacques>
2018-11-02 16:50:01 -0200

    Primeiro commit

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -3,11 +3,18 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
 module Handler.Home where
 
+import Text.Lucius
+import Text.Julius
 import Import
-import Network.HTTP.Types.Status
-import Database.Persist.Postgresql
+import Prelude
 
 getHomeR :: Handler Html
-getHomeR = undefined
+getHomeR = do
+		defaultLayout $ do 
+			addStylesheet $ StaticR css_bootstrap_css
+			toWidgetHead $(juliusFile "templates/homepage.julius")
+			toWidget $(luciusFile "templates/homepage.lucius")
+			$(whamletFile "templates/homepage.hamlet")
