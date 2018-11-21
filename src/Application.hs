5d85a7921171d5a7128be15c1675bdf985d3f1d2
Matheus Jacques <Matheus Jacques>
2018-11-21 00:36:39 -0200

    Adicionado os Handlers de Ranking

--- src/Application.hs
+++ src/Application.hs
@@ -43,6 +43,7 @@ import Handler.Login
 import Handler.Influencias
 import Handler.Super
 import Handler.Admin
+import Handler.Ranking
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
---
+++ src/Handler/Ranking.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Handler.Ranking where
+
+import Text.Lucius
+import Text.Julius
+import Import
+
