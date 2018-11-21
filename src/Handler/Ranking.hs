ff8bae125f7bd535cbd9e69bfb838f76661ae397
Matheus Jacques <Matheus Jacques>
2018-11-21 01:09:46 -0200

    Adicionado funções temporárias(placeholder) e os arquivos do front(placeholder)

--- src/Handler/Ranking.hs
+++ src/Handler/Ranking.hs
@@ -6,7 +6,21 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Handler.Ranking where
 
+import Import
 import Text.Lucius
 import Text.Julius
-import Import
+import Database.Persist.Sql
 
+widgetBootstrapLinks :: Widget
+widgetBootstrapLinks = $(whamletFile "templates/bootstrapLinks.hamlet")
+    
+getRankingR :: Handler Html
+getRankingR = do
+    defaultLayout $ do 
+        addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+        $(whamletFile "templates/ranking.hamlet")
+    
+postRankR :: RankingId -> Handler Html
+postRankR ranid = do 
+    runDB $ delete ranid
+    redirect RankingR
