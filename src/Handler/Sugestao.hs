95a10f2b21bbe27bcb0906d80ce67fb96ec64ad6
Matheus Luiz Jacques <ml.jacques@hotmail.com>
2018-12-08 20:37:59 +0000

    Criado a rota e o Painel de Sugestão

--- src/Handler/Sugestao.hs
+++ src/Handler/Sugestao.hs
@@ -9,6 +9,8 @@ module Handler.Sugestao where
 import Text.Lucius
 import Text.Julius
 import Import
+import Prelude (read)
+import Database.Persist.Sql
 
 widgetBootstrapLinks :: Widget
 widgetBootstrapLinks = $(whamletFile "templates/bootstrapLinks.hamlet")
@@ -42,3 +44,36 @@ postSugestaoR plaid = do
             |]
             redirect HomeR
         _ -> redirect HomeR
+        
+getPainelR :: Handler Html
+getPainelR = do
+    admin <- lookupSession "_ADM"
+    case admin of
+        Just _ -> do 
+            defaultLayout $ do 
+                addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+                toWidget $(luciusFile "templates/painelSugestaoAdm.lucius")
+                $(whamletFile "templates/painelSugestaoAdm.hamlet")
+        Nothing -> do
+            logado <- lookupSession "_PLA"
+            case logado of 
+                Just sessionPlayer -> do 
+                    dados <- return $ read $ unpack sessionPlayer
+                    player <- runDB $ selectFirst [PlayerEmail ==. (playerEmail dados), PlayerSenha ==. (playerSenha dados)] []
+                    case player of
+                        Just (Entity plaid _) -> do
+                            pid <- return $ plaid
+                            defaultLayout $ do 
+                                addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+                                toWidget $(luciusFile "templates/painelSugestaoLogado.lucius")
+                                $(whamletFile "templates/painelSugestaoLogado.hamlet")
+                        Nothing -> do
+                            defaultLayout $ do 
+                                addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+                                toWidget $(luciusFile "templates/painelSugestao.lucius")
+                                $(whamletFile "templates/painelSugestao.hamlet")
+                Nothing -> do
+                    defaultLayout $ do 
+                        addStylesheetRemote "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+                        toWidget $(luciusFile "templates/painelSugestao.lucius")
+                        $(whamletFile "templates/painelSugestao.hamlet")
